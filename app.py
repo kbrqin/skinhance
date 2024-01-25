16f8e00dd5f2a94c83f80e9d7f686a92acd7fdb3
kbrqin <70354257+kbrqin@users.noreply.github.com>
2024-01-25 03:32:21 -0500

    escaped the colon

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ Welcome to Skinhance! This app helps you find similar skincare products to a pro
 - The **Product Recommendations** list recommends products with similar benefits to the chosen product.
 - The **Similar Ingredients** list recommends products with similar ingredients to the chosen product.
 - The two lists are ordered from most similar to least similar, so the first few products are the most similar, but the further down the list you go, the less related to the chosen product they will be.
-- The two lists can be very similar, but this would make sense, since products with similar ingredients will have similar benefits!
+- The two lists can be very similar, but this would make sense, since products with similar ingredients will have similar benefits! 
 '''
 
 desc_expander.write(app_desc)
